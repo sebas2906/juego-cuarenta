379cab8de45426c126888c0e7c5fe245fd1a63be
sebas2906 <s.sebas33@gmail.com>
2021-06-02 16:25:15 -0500

    Arreglado Server

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  socketConfig: 'https://opmini.online:5000'//cambiar con el del server
+  socketConfig: 'http://opmini.online:5000'//cambiar con el del server
 };
